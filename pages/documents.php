cea5bb83f66b520d1425754d9186292f5410ca30
Jordan B <rbrewerjcksn49@gmail.com>
2025-01-22 18:33:24 -0500

    new documents

--- pages/documents.php
+++ pages/documents.php
@@ -2,8 +2,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Radiant Radios | Documents <title>
+<styles>
+@import url(https://fonts.googleapis.com/css?family=Roboto:400,100,300,500,700);
 
+body {
+  background: #EEEEF4;
+  color:#999;
+  font-family:Roboto;
+}
 
+h1{
+  font-weight:100;
+  font-size:27pt;
+  color:#E43;
+}
+
+p{font-weight:300;}
+
+.warning-content {
+	position:absolute;
+  top:25%;
+  width:100%;
+  height:300px;
+  text-align:center;
+  margin:0;
+  
+  
+}
+</styles>
+    <div class="warning-content">
+<h1>New Document System  Under Construction</h1>
+
+<p>
+  Please forgive the inconvenience. <br />
+  We are currently initializing our brand new site.
+</p>
+<p>
+  It's okay, we're excited too!
+</p>
+
+</div>
 
 <?php include template/H.php'; ?>
 
