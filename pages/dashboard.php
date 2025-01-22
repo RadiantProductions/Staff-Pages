8eccfded388d18bf3e95333898957dcdeaed3768
Jordan B <rbrewerjcksn49@gmail.com>
2025-01-22 01:05:45 -0500

    Update dashboard.php
    Signed-off-by: Jordan B <rbrewerjcksn49@gmail.com>

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -1,1 +1,7 @@
-NOT WORKIN
+    <!DOCTYPE html>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header Only</title>
+
+
+<?php include('template/H.php'); ?>
